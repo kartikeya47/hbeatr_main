61850d13644af037433d188d150d3279c9d71f36
adi-py9 <95119075+adi-py9@users.noreply.github.com>
2021-12-05 22:41:28 +0530

    going live on server

--- deploy/settings.py
+++ deploy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2o6vn-##%g48b&c_1pyex&rn=gwgwoo_4sb4g9!c+mc49nt((@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hbeatr-adv.herokuapp.com','127.0.0.1']
 
 
 # Application definition
